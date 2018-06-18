d3a59e80266ba5bbdd6ad3aa6b1b39c3a7f6469e
Florian Magin <8415354+fmagin@users.noreply.github.com>
2018-06-18 13:54:29 +0200

    Make stackview a functioning plugin

--- angrcli/plugins/stackview.py
+++ angrcli/plugins/stackview.py
@@ -13,6 +13,10 @@ class Stack(SimStatePlugin):
     def set_state(self, state):
         super(Stack, self).set_state(state)
 
+    @SimStatePlugin.memo
+    def copy(self, memo):
+        return Stack()
+
 
     def element_count(self):
         return self.state.solver.eval( (self.state.arch.initial_sp - self.state.regs.sp) / self.state.arch.bytes)

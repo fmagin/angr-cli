589941162482dd9dd3def41299a118cabba1ac97
Florian Magin <8415354+fmagin@users.noreply.github.com>
2018-06-18 16:28:24 +0200

    Implement basic explore shell

--- angrcli/interaction/explore.py
+++ angrcli/interaction/explore.py
@@ -1,10 +1,13 @@
 
 from cmd import Cmd
 
+import os
 
 
 class ExploreInteractive(Cmd, object):
 
+    intro = "Dropping into angr shell"
+    prompt = "explore>"
 
     def __init__(self, proj, state):
         super(ExploreInteractive, self).__init__()
@@ -24,6 +27,51 @@ class ExploreInteractive(Cmd, object):
         print "Quitting."
         raise SystemExit
 
+    def do_print(self, arg):
+        pick = int(arg)
+        self.simgr.active[pick].context_view.pprint()
 
-    def do_step(self):
+
+    def do_stepi(self, args):
+        if len(self.simgr.active) == 1:
+            self.simgr.step(num_inst=1)
+            print("\033[H\033[J")
+            self.simgr.one_active.context_view.pprint()
+        elif len(self.simgr.active) > 1:
+            idx = 0
+            for state in self.simgr.active:
+                print state.context_view.pstr_branch_info(idx)
+                idx += 1
+
+    def do_step(self, args):
+        if len(self.simgr.active) == 1:
+            self.simgr.step()
+            print("\033[H\033[J")
+            self.simgr.one_active.context_view.pprint()
+        elif len(self.simgr.active) > 1:
+            idx = 0
+            for state in self.simgr.active:
+                print state.context_view.pstr_branch_info(idx)
+                idx += 1
+
+    def do_s(self, args):
+        self.do_step(args)
+
+    def do_run(self, args):
+        self.simgr.run(until=lambda s: len(s.active) != 1)
+        idx = 0
+        for state in self.simgr.active:
+            print state.context_view.pstr_branch_info(idx)
+            idx += 1
+
+
+    def do_pick(self, arg):
+        pick = int(arg)
+        ip = self.simgr.active[pick].regs.ip
+        print("Picking state with ip: " + (str(ip)))
+        self.simgr.move(from_stash='active',
+                   to_stash="stashed",
+                   filter_func=lambda x: x.solver.eval(ip != x.regs.ip))
         self.simgr.step()
+        print("\033[H\033[J")
+        self.simgr.one_active.context_view.pprint()

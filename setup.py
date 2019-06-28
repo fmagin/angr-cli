c8306b4fd2964675f7d8604c491463e2a2d25f14
Andrea Fioraldi <andreafioraldi@gmail.com>
2019-06-28 14:12:57 +0200

    Fix broken setup.py
    Without this fix only the first level package is installed, so only the angrcli/__init__.py file.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='angrcli',
     version='0.1dev',
-    packages=['angrcli',],
+    packages=find_packages(),
     license='MIT',
     long_description='none',
 )

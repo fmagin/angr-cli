0a4af1432c75c7951874fd0ffb78f701816f6b5e
Florian Magin <fmagin@users.noreply.github.com>
2021-12-28 14:17:28 +0100

    Release version 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='angrcli',
-    version='1.1.1',
+    version='1.2.0',
     packages=find_packages(exclude=['tests.*', 'tests', 'example.*', 'example']),
     include_package_data=True,
     license='MIT',
